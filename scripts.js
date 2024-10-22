33943562efc0d642b8c805d1cf5176b510ee5741
Olosko23 <oloogeorge633@gmail.com>
2024-10-22 09:27:18 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for contacting Avodal Technology! We will get back to you soon.');
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  alert('Thank you for contacting Avodal Technology! We will get back to you soon.');
+});
+
+const backToTopButton = document.getElementById('backToTop');
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    backToTopButton.style.display = "block"; // Show the button
+  } else {
+    backToTopButton.style.display = "none"; // Hide the button
+  }
+};
+
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopButton.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Smooth scroll effect
   });
-  
+});
