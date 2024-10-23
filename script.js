03ebb453281bd010a8403220f49bb341cacbc503
Olosko23 <oloogeorge633@gmail.com>
2024-10-23 17:30:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-  alert('Thank you for contacting Avodal Technology! We will get back to you soon.');
+
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  const email = document.getElementById("email").value;
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+
+  if (!email || !name || !message) {
+    alert("All fields are required!");
+    e.preventDefault();
+  }
 });
+
 
 const backToTopButton = document.getElementById('backToTop');
 
 window.onscroll = function () {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    backToTopButton.style.display = "block"; // Show the button
+    backToTopButton.style.display = "block";
   } else {
-    backToTopButton.style.display = "none"; // Hide the button
+    backToTopButton.style.display = "none";
   }
 };
 
 
-// When the user clicks on the button, scroll to the top of the document
 backToTopButton.addEventListener('click', function () {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth' // Smooth scroll effect
+    behavior: 'smooth'
   });
 });
 
 
 window.addEventListener('scroll', function () {
   const body = document.body;
-  if (window.scrollY > 50) { // Adjust the number as needed for the scroll trigger
+  if (window.scrollY > 50) {
     body.classList.add('scrolled');
   } else {
     body.classList.remove('scrolled');
   }
 });
-
